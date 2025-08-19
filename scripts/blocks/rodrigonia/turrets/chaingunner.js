74d25661ca93285776e3c077efbb780256387b32
Rodrigo Rodrigues <rodrigo.rodrigues.rr.acc2@gmail.com>
2025-08-19 15:47:42 +0100

    fuck my life

--- scripts/blocks/rodrigonia/turrets/chaingunner.js
+++ scripts/blocks/rodrigonia/turrets/chaingunner.js
@@ -1,4 +1,4 @@
-let chaingunner = extend(ItemTurret, "chaingunner", {
+/* let chaingunner = extend(ItemTurret, "chaingunner", {
     setBars(){
         this.super$setBars();
         this.addBar("heat", func(e =>
@@ -14,16 +14,19 @@ chaingunner.explosionRadius = 18;
 chaingunner.explosionDamage = 10000;
 chaingunner.buildType = () => extend(ItemTurret.ItemTurretBuild, chaingunner, {
     update(){
-        /*
         for (let i=0; i<Groups.bullet.size(); i++){
             if(Groups.bullet.index(i).owner == this){
                 this.heat += 0.005
             }
         }
-        */
         if(this.heat > 0.999){
             this.kill();
+        }
+        if(this.coolant > 50){
+            this.heat -= 0.5
+            this.coolant -= 0.5
         }
         this.super$update();
     }
 })
+*/
